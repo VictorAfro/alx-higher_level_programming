588c85a7490cd9817f705239368df3d99ca2adb8
VictorAfro <victorgit82@gmail.com>
2023-03-13 19:14:47 +0000

    submit

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-print_list_integer.py
+
+
+def print_list_integer(my_list=[]):
+  """Retrive an element from a list."""
+  if idx < 0 or idx > (len(my_list) - 1):
+	return None
+  return (my_list[idx])
